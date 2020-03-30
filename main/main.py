3a0ab7d9d37612f1004a00b666c8460efeef077a
alyssonamaral <alysson.amaral@hotmail.com>
2020-03-29 22:31:49 -0400

    code

--- main/main.py
+++ main/main.py
@@ -1,1 +1,15 @@
 
+import random
+items=[]
+i=0
+print ('Press ENTER in a null item to finish the list and draw an item')
+while True:
+    i+=1
+    item=input('Enter item %d: '%i)
+    if item=='':
+        break
+    items.append(item)
+print(f'The items in the list are:{items}')
+print(f'The chosen one is: {random.choice(items)}')
+perc = 100/len(items)
+print ('The probability were:',perc,'%')
